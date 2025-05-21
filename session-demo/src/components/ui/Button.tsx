549196678cdb5652a7aa4ed0b8ff8b4b27c73b63
P4ST4S <antoine.rospars@epitech.eu>
2025-05-21 11:55:35 +0200

    Update Button component to include active state styling

--- session-demo/src/components/ui/Button.tsx
+++ session-demo/src/components/ui/Button.tsx
@@ -13,7 +13,7 @@ const Button = ({
 }: ButtonProps) => {
   return (
     <button
-      className={`flex flex-col justify-center items-center gap-2 w-[322px] h-[48px] shrink-0 rounded-[12px] cursor-pointer bg-[#11E5C5] hover:bg-[#7dffeb] text-[#3C3C40] text-center font-poppins text-sm font-medium leading-[110%] ${className}`}
+      className={`flex flex-col justify-center items-center gap-2 w-[322px] h-[48px] shrink-0 rounded-[12px] cursor-pointer bg-[#11E5C5] hover:bg-[#7dffeb] text-[#3C3C40] text-center font-poppins text-sm font-medium leading-[110%] active:bg-[#11e5c57c] ${className}`}
       onClick={onClick}
       disabled={disabled}
     >
