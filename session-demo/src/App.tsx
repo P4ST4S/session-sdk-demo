9d684f129796f9b8a0df0039a6611d5316ea31e9
P4ST4S <antoine.rospars@epitech.eu>
2025-07-23 10:54:47 +0200

    Update session ID in App component to new value

--- session-demo/src/App.tsx
+++ session-demo/src/App.tsx
@@ -2,7 +2,7 @@ import useSession from "./hooks/useSession";
 // import useSession from "datakeen-session-react";
 
 function App() {
-  const { SessionComponent } = useSession("22f5a48b1a1008b684379291");
+  const { SessionComponent } = useSession("1f53fce4a59791de274c841d");
   return <div>{SessionComponent}</div>;
 }
 
